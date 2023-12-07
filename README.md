9606301b1dfa425e1abec5b05951f04675c82823
Elbia Simone Buglio <elbiasimone@hotmail.com>
2023-12-06 22:15:15 -0300

    aula_06_12

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 1000 DEVs
+
+## Segunda aula sobre git. testes unitarios  e nodeJS
---
+++ src/tests/sum.test.js
@@ -0,0 +1,9 @@
+const {soma } = require(`../utils/sum`)
+
+test(`Showld return a sum of two numbers`, () =>  {
+    expect(soma(5, 5)).toBe(10)
+})
+
+test (`Deve retornar a divisao de dois numeros`, () => {
+
+})
