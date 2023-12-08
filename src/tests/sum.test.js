916a5052dd80e4c377e21fec8befd98d8ccb47b9
Elbia Simone Buglio <elbiasimone@hotmail.com>
2023-12-07 22:08:08 -0300

    Aula 07/12

--- src/tests/sum.test.js
+++
@@ -1,9 +0,0 @@
-const {soma } = require(`../utils/sum`)
-
-test(`Showld return a sum of two numbers`, () =>  {
-    expect(soma(5, 5)).toBe(10)
-})
-
-test (`Deve retornar a divisao de dois numeros`, () => {
-
-})
